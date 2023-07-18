26ff86dab1221a4cc48d386f48b18e36bef24115
Dwaipayan25 <dwaipayan.dehury@gmail.com>
2023-07-18 11:06:07 +0530

    Done

---
+++ frontend/src/components/Faucet.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { ethers } from "ethers"
+import { Container, Form, Button, Alert } from 'react-bootstrap';
+
+const toWei = (n) => {
+    return ethers.utils.parseEther(n.toString());
+  };
+
+const CryptoFaucet = ({account,FRAX}) => {
+  const [amount, setAmount] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleMintTokens = async() => {
+    const mint = await FRAX.mint(toWei(amount));
+    await mint.wait();
+    setMessage(`Minted ${amount} tokens successfully!`);
+    setAmount('');
+  };
+
+  return (
+    <Container className="mt-4">
+      <h2 className="mb-4">FRAX Faucet</h2>
+      <p>This is only for testing purpose, it doesnot provide real FRAX coins</p>
+      <Form.Group className="mb-3">
+        <Form.Label><h3>Enter Amount:</h3></Form.Label>
+        <Form.Control
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+      </Form.Group>
+      <Button
+        variant="primary"
+        onClick={handleMintTokens}
+        disabled={!amount || amount <= 0}
+      >
+        Mint Tokens
+      </Button>
+      {message && (
+        <Alert variant="success" className="mt-3">
+          {message}
+        </Alert>
+      )}
+    </Container>
+  );
+};
+
+export default CryptoFaucet;
